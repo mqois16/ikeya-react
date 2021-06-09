a0c8c1e2287114ecad63e86f24e0b8ca263a41bb
mqois16 <qoisoctava@gmail.com>
2021-06-09 11:21:53 +0700

    useScrollAnchor useModalDom

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,13 +1,17 @@
 import React from "react";
 import Header from "parts/Header";
-import Hero from "parts/Hero";
+import Hero from "parts/HomePages/Hero";
 import BrowseRoom from "parts/HomePages/BrowseRoom";
 import JustArrived from "parts/HomePages/JustArrived";
 import SiteMap from "parts/SiteMap";
 import Clients from "parts/Clients";
 import Footer from "parts/Footer";
-
+import useScrollAnchor from "helpers/hooks/useScrollAnchor";
+import useModalDom from "helpers/hooks/useModalDom";
 function HomePage() {
+  //hooks
+  useScrollAnchor();
+  useModalDom();
   return (
     <>
       <Header theme="white" position="absolute"></Header>
